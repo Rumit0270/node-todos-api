2fabdf06e1dd5d839ed50fcacd0252d0f8aa8bd2
Ichigo <rumit0270@gmail.com>
2018-09-26 23:16:48 +0545

    added tests for GEt /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,10 +6,17 @@ const {Todo} = require('./../models/todo');
 const {User} = require('./../models/user');
 
 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
 
 //add testing lifecycle
 beforeEach((done) => {
-  Todo.deleteMany({}).then(() => done());
+  Todo.deleteMany({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 
@@ -29,7 +36,7 @@ describe('POST /todos', () => {
             return done(err);
           }
 
-          Todo.find().then((todos) => {
+          Todo.find({text}).then((todos) => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
             done();
@@ -49,9 +56,22 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2); // 2 because we added two docs
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+
+describe('GET /todos', () => {
+  it('should return two todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
