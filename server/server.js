f46ed04926af9dcda6afee2eeb30d70dfbf5f4ac
Ichigo <rumit0270@gmail.com>
2018-09-26 22:36:55 +0545

    added test scripts for /todos route

--- server/server.js
+++ server/server.js
@@ -28,6 +28,8 @@ app.listen(3000, () => {
   console.log('Server running at port 3000');
 });
 
+module.exports = {app};
+
 
 
 
---
+++ server/tests/server.test.js
@@ -0,0 +1,57 @@
+const request = require('supertest');
+const expect = require('expect');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+
+
+
+//add testing lifecycle
+beforeEach((done) => {
+  Todo.deleteMany({}).then(() => done());
+});
+
+
+describe('POST /todos', () => {
+  it('should create a new todo', (done) => {
+    var text = 'Test todo text';
+
+    request(app)
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+          if(err) {
+            return done(err);
+          }
+
+          Todo.find().then((todos) => {
+            expect(todos.length).toBe(1);
+            expect(todos[0].text).toBe(text);
+            done();
+          }).catch((e) => done(e));
+      });
+  });
+
+
+  it('should not create todo with invalid body data', (done) => {
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
