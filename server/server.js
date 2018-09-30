20ee375f779e9c224e28bd309b68b9faacc05b71
Ichigo <rumit0270@gmail.com>
2018-09-30 11:31:57 +0545

    added route to PATCH /todos/id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
+const _ = require('lodash');
+const express = require('express');
 // bodyParser is used to convert JSON in JS object
-var bodyParser = require('body-parser');
+const  bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -67,6 +68,36 @@ app.delete('/todos/:id', (req, res) => {
       }
       return res.send({todo});
   }, (err) => {
+    res.status(400).send();
+  });
+
+});
+
+app.patch('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  // ensure only required property aree updated
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send({});
+  }
+  
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+      body.completed = false;
+      body.completedAt = null;
+  }
+  console.log(body);
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
     res.status(400).send();
   });
 
